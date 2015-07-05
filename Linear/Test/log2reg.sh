4c19f3800d64c1f931b8d71000b6a5bc8b056285
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2015-07-05 08:49:07 +0200

    Moved the log2reg.sed file into the scripts folder

--- Linear/Test/log2reg.sh
+++ Linear/Test/log2reg.sh
@@ -2,7 +2,7 @@
 # $Id$
 # This script creates new reg-files from the specified log-files.
 for file in $*; do if [ -f $file ]; then found=yes; echo $file
-sed -f ../../../../test/log2reg.sed $file | sed \
+sed -f ../../../../scripts/log2reg.sed $file | sed \
 '1 s/^.*LinEl //;1 s/ *-vtf *1//;1 s/ *-nviz *[1-9]//;1 s/ *-n[uvw] *[1-9]//g;1 s/ *-hdf5//; 1 a\
 ' > `basename $file .log`.reg
 fi; done
