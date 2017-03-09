f768728a3a2ac58d8e698c6360699e0c0c43e94a
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2017-03-09 21:57:12 +0100

    Removed the symbolic links to inp-files. Does not work so well when cloning in a Windows share. Instead, the run script creates those links temporarily.

--- Linear/Test/run.sh
+++ Linear/Test/run.sh
@@ -14,12 +14,20 @@ run () {
 # This function runs a simulation with the specified options,
 # pipes the terminal output to a log-file, and compares it
 # with a previous simulation stored in the Reference folder.
-  inp=$1
-  log=`basename $1 .inp`.log
+  if `echo $2 | grep -q '^\-'`; then
+    inp=$1
+    log=`basename $1 .inp`.log
+  else
+    inp=$2.inp
+    log=$2.log
+    ln -s $1 $inp
+    shift
+  fi
   echo Running $inp ...
   shift
   time -f "real time %E, user time %U, system time %S" \
   ../../../Release/bin/$mysim $inp $* > $log
+  if [ -h $inp ]; then rm $inp; fi
   if [ ! -e Reference/$log ]; then
     mv $log Reference
   elif cmp -s $log Reference/$log; then
@@ -37,27 +45,27 @@ if [ ! -d Reference ]; then mkdir Reference; fi
 # Format: run <inputfile> [<options>]
 #####################################
 
-run Hole2D-NURBS.inp    -2Dpstrain -vtf 1 -nviz 4 -hdf5
-run Hole2D-Lagrange.inp -2Dpstrain -vtf 1 -lagrange
-run Hole2D-Spectral.inp -2Dpstrain -vtf 1 -spectral
+run Hole2D-p3.inp Hole2D-NURBS    -2Dpstrain -vtf 1 -nviz 4 -hdf5
+run Hole2D-p3.inp Hole2D-Lagrange -2Dpstrain -vtf 1 -lagrange
+run Hole2D-p3.inp Hole2D-Spectral -2Dpstrain -vtf 1 -spectral
 
-run Cylinder-NURBS.inp    -checkRHS -vtf 1 -nviz 5 -hdf5
-run Cylinder-Lagrange.inp -checkRHS -vtf 1 -lagrange -nGauss 5
-run Cylinder-Spectral.inp -checkRHS -vtf 1 -spectral
+run Cylinder-p4.inp Cylinder-NURBS    -checkRHS -vtf 1 -nviz 5 -hdf5
+run Cylinder-p4.inp Cylinder-Lagrange -checkRHS -vtf 1 -lagrange -nGauss 5
+run Cylinder-p4.inp Cylinder-Spectral -checkRHS -vtf 1 -spectral
 
 run Cylinder-Axisymm.inp -2Daxisymm -vtf 1 -nviz 5 -hdf5
 
 eigOpt="-eig 5 -nev 20 -ncv 40 -shift 2.0e-4"
-run SquarePlate-Splines.inp  $eigOpt -nGauss 3 -vtf 1 -nviz 3
-run SquarePlate-Lagrange.inp $eigOpt -nGauss 3 -vtf 1 -lagrange
-run SquarePlate-Spectral.inp $eigOpt -nGauss 3 -vtf 1 -spectral
+run SquarePlate-p2.inp SquarePlate-Splines  $eigOpt -nGauss 3 -vtf 1 -nviz 3
+run SquarePlate-p2.inp SquarePlate-Lagrange $eigOpt -nGauss 3 -vtf 1 -lagrange
+run SquarePlate-p2.inp SquarePlate-Spectral $eigOpt -nGauss 3 -vtf 1 -spectral
 
-run PipeJoint-vibration.inp -vtf 1 -nu 7 -nv 7 -free -eig 4 -nev 16 -ncv 32
-run PipeJoint-NURBS.inp     -vtf 1 -nu 7 -nv 7 -hdf5
-run PipeJoint-Lagrange.inp  -vtf 1 -lagrange
+run PipeJoint.inp PipeJoint-vibration -vtf 1 -nu 7 -nv 7 -free -eig 4 -nev 16 -ncv 32
+run PipeJoint.inp PipeJoint-NURBS     -vtf 1 -nu 7 -nv 7 -hdf5
+run PipeJoint.inp PipeJoint-Lagrange  -vtf 1 -lagrange
 
-run NavierPress_p2.inp -KL -nGauss 2 -vtf 1 -nviz 7
-run NavierPress_p3.inp -KL -nGauss 3 -vtf 1 -nviz 7 -hdf5
-run NavierPart_p2.inp  -KL -nGauss 2 -vtf 1 -nviz 7
-run NavierPart_p3.inp  -KL -nGauss 3 -vtf 1 -nviz 7 -hdf5
-run NavierPoint_p3.inp -KL -nGauss 3 -vtf 1 -nviz 5 -hdf5
+run NavierPress_p2.inp -2DKL -nGauss 2 -vtf 1 -nviz 7
+run NavierPress_p3.inp -2DKL -nGauss 3 -vtf 1 -nviz 7 -hdf5
+run NavierPart_p2.inp  -2DKL -nGauss 2 -vtf 1 -nviz 7
+run NavierPart_p3.inp  -2DKL -nGauss 3 -vtf 1 -nviz 7 -hdf5
+run NavierPoint_p3.inp -2DKL -nGauss 3 -vtf 1 -nviz 5 -hdf5
