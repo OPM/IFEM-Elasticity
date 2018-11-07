67539ffbcd138a095dcdb1740938394e3deb7edd
Kjetil Andre Johannessen <kjetijo@gmail.com>
2018-11-07 10:24:51 +0100

    Added: nx/ny kewords to geometry tag for splitting singlepatch to multipatch

--- SIMElasticity.h
+++ SIMElasticity.h
@@ -26,6 +26,7 @@
 #include "Functions.h"
 #include "Utilities.h"
 #include "tinyxml.h"
+#include "SIMMultiPatchModelGen.h"
 
 typedef std::vector<Material*> MaterialVec; //!< Convenience declaration
 
@@ -37,12 +38,13 @@ typedef std::vector<Material*> MaterialVec; //!< Convenience declaration
   and some property initialization methods of the parent class.
 */
 
-template<class Dim> class SIMElasticity : public Dim
+template<class Dim> class SIMElasticity : public SIMMultiPatchModelGen<Dim>
 {
 public:
   //! \brief Default constructor.
   //! \param[in] checkRHS If \e true, ensure the model is in a right-hand system
-  explicit SIMElasticity(bool checkRHS = false) : Dim(Dim::dimension,checkRHS)
+  explicit SIMElasticity(bool checkRHS = false) :
+    SIMMultiPatchModelGen<Dim>(Dim::dimension,checkRHS)
   {
     myContext = "elasticity";
     aCode = bCode = 0;
