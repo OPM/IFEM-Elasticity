9d57d5eb579eaca7aff6989fee29f58948d3ff88
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2023-09-23 13:38:56 +0200

    The script log2reg.sh may now be invoked from other places as well

--- Linear/Test/log2reg.sh
+++ Linear/Test/log2reg.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # $Id$
 # This script creates new reg-files from the specified log-files.
+TEST_DIR=`dirname $0`
+if [ "$TEST_DIR" == "." ]; then
+SCRIPT_DIR=../../../../scripts
+else
+SCRIPT_DIR=$TEST_DIR/../../../../scripts
+fi
 for file in $*; do if [ -f $file ]; then found=yes; echo $file
-sed -f ../../../../scripts/log2reg.sed $file | sed \
+sed -f $SCRIPT_DIR/log2reg.sed $file | sed \
 '1 s/^.*LinEl //;1 s/ *-vtf *1//;1 s/ *-nviz *[1-9]//;1 s/ *-n[uvw] *[1-9]//g;1 s/ *-hdf5//; 1 a\
 ' > `basename $file .log`.reg
 fi; done
