4da882f990c3d82d04d58065a81ad6f3acde7f0e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-09-19 15:50:09 +0200

    convert unit-tests to catch

--- Linear/Test/TestStaticCondensation.C
+++ Linear/Test/TestStaticCondensation.C
@@ -16,36 +16,39 @@
 #include "SAM.h"
 #include <array>
 
-#include "gtest/gtest.h"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+using Catch::Matchers::WithinRel;
 
 
-TEST(TestSIMLinEl2D, StaticCondensation)
+TEST_CASE("TestSIMLinEl.StaticCondensation2D")
 {
   SIMLinEl2D fullModel(nullptr,false,false), scModel(nullptr,false,false);
   Vectors    displ(2);
 
   // Initialize the full model from input file
-  ASSERT_TRUE(fullModel.read("SSmembrane-p1.xinp"));
-  ASSERT_TRUE(fullModel.preprocess());
+  REQUIRE(fullModel.read("SSmembrane-p1.xinp"));
+  REQUIRE(fullModel.preprocess());
 
   // Assemble and direct solve the full equation system
   fullModel.setMode(SIM::STATIC);
   fullModel.setQuadratureRule(fullModel.opt.nGauss[0],true,true);
   fullModel.initSystem(LinAlg::SPARSE);
-  ASSERT_TRUE(fullModel.assembleSystem());
-  ASSERT_TRUE(fullModel.solveSystem(displ,1));
+  REQUIRE(fullModel.assembleSystem());
+  REQUIRE(fullModel.solveSystem(displ,1));
   std::cout <<"Full Solution:"<< displ.front();
 
   // Initialize the model to be solved by static condensation
   SIMbase::ignoreDirichlet = true;
-  ASSERT_TRUE(scModel.read("SSmembrane-p1.xinp"));
-  ASSERT_TRUE(scModel.preprocess());
+  REQUIRE(scModel.read("SSmembrane-p1.xinp"));
+  REQUIRE(scModel.preprocess());
 
   // Assemble equation system and perform static condensation
   Matrix Kred;
   Vector Rred;
   scModel.opt.num_threads_SLU = -1; // To avoid pre-assembly
-  ASSERT_TRUE(scModel.staticCondensation(Kred,Rred));
+  REQUIRE(scModel.staticCondensation(Kred,Rred));
 #ifdef INT_DEBUG
   std::cout <<"Kred:"<< Kred;
   std::cout <<"Rred:"<< Rred;
@@ -68,7 +71,7 @@ TEST(TestSIMLinEl2D, StaticCondensation)
   // Solve the condensed system
   DenseMatrix Ksup(Kred);
   StdVector   Rsup(Rred);
-  ASSERT_TRUE(Ksup.solve(Rsup));
+  REQUIRE(Ksup.solve(Rsup));
   std::cout <<"Superelement Solution:"<< Rsup;
 
   // Find global DOF numbers of the "supernodes" 106 and 107
@@ -80,25 +83,25 @@ TEST(TestSIMLinEl2D, StaticCondensation)
   // Compare the solutions, they should be identical
   int idof, i = 0;
   for (idof = dofs1.first; idof <= dofs1.second; idof++, i++)
-    EXPECT_NEAR(displ.front()(idof), Rsup[i], 1.0e-8);
+    REQUIRE_THAT(displ.front()(idof), WithinRel(Rsup[i], 1e-12));
   for (idof = dofs2.first; idof <= dofs2.second; idof++, i++)
-    EXPECT_NEAR(displ.front()(idof), Rsup[i], 1.0e-8);
+    REQUIRE_THAT(displ.front()(idof), WithinRel(Rsup[i], 1e-12));
 
   // Check that the recovery matrix is valid
   FILE* fd = fopen("SSmembrane-p1.rec","rb");
-  ASSERT_FALSE(fd == nullptr);
+  REQUIRE(fd != nullptr);
 
   size_t len = 0, n1, n2;
   char* header = nullptr;
-  ASSERT_GT(getline(&header,&len,fd),0);
-  ASSERT_FALSE(strstr(header,"#IFEM recovery matrix:") == nullptr);
-  EXPECT_EQ(sscanf(header+22,"%zu%zu",&n1,&n2),2);
+  REQUIRE(getline(&header,&len,fd) > 0);
+  REQUIRE(strstr(header,"#IFEM recovery matrix:") != nullptr);
+  REQUIRE(sscanf(header+22,"%zu%zu",&n1,&n2) == 2);
   std::cout << header << n1 <<" "<< n2;
   free(header);
 
   Matrix Rmat(n1,n2);
   for (size_t c = 0; c < n2; c++)
-    EXPECT_EQ(fread(Rmat.ptr(c),sizeof(double),n1,fd),n1);
+    REQUIRE(fread(Rmat.ptr(c),sizeof(double),n1,fd) == n1);
   fclose(fd);
 #if INT_DEBUG > 2
   std::cout << Rmat;
@@ -107,9 +110,9 @@ TEST(TestSIMLinEl2D, StaticCondensation)
 #endif
 
   // Recover internal displacements and verify against the full solution
-  ASSERT_TRUE(scModel.recoverInternals(Rsup,displ.back()));
+  REQUIRE(scModel.recoverInternals(Rsup,displ.back()));
   std::cout <<"Recovered Solution:"<< displ.back();
-  ASSERT_EQ(displ.front().size(),displ.back().size());
+  REQUIRE(displ.front().size() == displ.back().size());
   for (size_t j = 0; j < displ.front().size(); j++)
-    EXPECT_NEAR(displ.front()[j], displ.back()[j], 1.0e-8);
+    REQUIRE_THAT(displ.front()[j], WithinRel(displ.back()[j], 1e-11));
 }
