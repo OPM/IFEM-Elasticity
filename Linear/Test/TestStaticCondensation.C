d5cca20c4e91309b58a2756b0686f2118132fdce
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-09-30 09:21:37 +0200

    add support for catch2 v2

--- Linear/Test/TestStaticCondensation.C
+++ Linear/Test/TestStaticCondensation.C
@@ -16,10 +16,7 @@
 #include "SAM.h"
 #include <array>
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
-
-using Catch::Matchers::WithinRel;
+#include "Catch2Support.h"
 
 
 TEST_CASE("TestSIMLinEl.StaticCondensation2D")
